1bf6d52561e316ed1dd78d02231714f1530b062a
Othmane Necib <othmane.necib@gmail.com>
2017-08-29 13:51:26 +0200

    Add files via upload

--- helpers/spec/jqcomments.spec.js
+++ helpers/spec/jqcomments.spec.js
@@ -6,8 +6,8 @@ describe('jquery', () => {
     it('should display nice message when no comments are availble', () => {
         setFixtures('<div id="comments"></div>');
         var comments = $("#comments");
-        comments.JQComments();
-        expect(comments.html()).toBe("Be the first one to comment");
+        comments.jQComments();
+        expect(comments.text()).toBe("Be the first one to comment...");
     });
     
     
